6ff58ca09a8898c53820e64807d589e53af063a4
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-22 10:37:28 +0500

    All everything is ok!

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean condition = false;
+        if (condition){
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean condition = true;
+        if (condition){
+            System.out.println(true);
+        }
     }
 }
